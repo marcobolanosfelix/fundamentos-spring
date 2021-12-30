1d7c348037875b532250d22aed69558a6afde4d1
marcobolanosfelix <marcobf1996@gmail.com>
2021-12-30 16:36:31 -0700

    Agregando seguridad en las peticiones con JWT

---
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -0,0 +1,20 @@
+package com.platzi.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    private static final String KEY = "Pl4tz1";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*10)) //10 horas de duracion con la llave
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+
+}
